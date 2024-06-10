be6442ebec39115acde2b16d5caed1974cd0e816
alfianchandra2607 <alfianchandra783@gmail.com>
2024-06-10 18:36:44 +0700

    yang terbaru

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KategoriController;
-
+//ini untuk login
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
